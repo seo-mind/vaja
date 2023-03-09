85b0a962ac8e8c0953d6723f103f4ce63f74e43e
seo-mind <58778024+seo-mind@users.noreply.github.com>
2023-03-09 22:33:15 +0900

    왜 안될까

--- demo/src/main/java/com/example/demo/web/MemberController.java
+++ demo/src/main/java/com/example/demo/web/MemberController.java
@@ -5,13 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.TbMemberTest;
 import com.example.demo.service.MemberService;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
 public class MemberController {
@@ -20,7 +21,7 @@ public class MemberController {
 	MemberService memberService;
 	
 	
-    @GetMapping(value = "/insMember")
+    @PostMapping(value = "/signup")
     public ResponseEntity<TbMemberTest> insMember(TbMemberTest tmt){
     	
         memberService.join(tmt);
