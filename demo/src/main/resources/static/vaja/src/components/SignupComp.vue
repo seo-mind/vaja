0b2c8a2bf91c9c9efbd605939fc00245962f9868
seo-mind <58778024+seo-mind@users.noreply.github.com>
2023-03-11 11:44:58 +0900

    Vue + Spring 데이터 전송

--- demo/src/main/resources/static/vaja/src/components/SignupComp.vue
+++ demo/src/main/resources/static/vaja/src/components/SignupComp.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <div class="input-group mb-3">
-      <input type="text" :model="name" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+      <input type="text" v-model="name" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
     </div>
-
+    <div class="input-group mb-3">
+      <input type="text" v-model="memberId" class="form-control" placeholder="ID" aria-label="ID" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+      <input type="text" v-model="email" class="form-control" placeholder="ID" aria-label="ID" aria-describedby="basic-addon1">
+    </div>
     <button @click="fnSignUp">회원 가입</button>
   </div>
 
@@ -16,16 +21,17 @@ export default {
   data(){
     return {
       name :'',
-
+      memberId :'',
+      email :'',
     }
   },
   methods:{
     fnSignUp(){
-      let data = {name : '전달데이터'
-                , memberId:'testId'
-                  }
-      axios.post('http://localhost/api/signup',JSON.stringify(data) , {
-        headers: { "Content-Type": `application/json`}
+      let data = {name : this.name
+                 ,memberId : this.memberId 
+                 ,email : this.email
+                 }
+      axios({url :'http://localhost/api/signup',method:'post',params : data 
       }).then( 결과 => {
        
         console.log(결과);
